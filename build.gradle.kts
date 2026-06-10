9cf6ad16a2ab13fdb95e6b34f2fb723ab0867a68
miles <miles@adnuntius.com>
2026-06-10 17:57:50 +1000

    Release v0.11.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,7 @@ compose.desktop {
             // must include java.management or opening any repo throws NoClassDefFoundError.
             modules("java.management")
             packageName = "nop"
-            packageVersion = "0.10.0"
+            packageVersion = "0.11.0"
             description = "Desktop editor and change reviewer"
             vendor = "iondrive"
             copyright = "Copyright (c) 2026 iondrive. All rights reserved."
